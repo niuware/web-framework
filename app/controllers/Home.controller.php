cef1d643129a6424a3e299b1d1d8141b560b1200
niuware <dev@niuware.com>
2017-06-07 18:04:25 +0900

    Added error control based on Exceptions. Optimized Controller core class.
    - Now is easier to render a controller method.
    - Removed unecessary HtmlResponse class.
    - Added the FrameworkException class.

--- app/controllers/Home.controller.php
+++ app/controllers/Home.controller.php
@@ -4,12 +4,11 @@ namespace Niuware\WebFramework\Controllers;
     
 use Niuware\WebFramework\Controller;
 use Niuware\WebFramework\HttpRequest;
-use Niuware\WebFramework\HtmlResponse;
 
 final class Home extends Controller {
     
     public function getHome(HttpRequest $request) {
         
-        HtmlResponse::getInstance()->render($this);
+        return $this->render();
     }
 }
