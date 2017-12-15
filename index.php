4c660168db4d31de674f101ac176f0ad85e30c58
niuware <dev@niuware.com>
2017-12-15 16:38:53 +0900

    Updated comments

--- index.php
+++ index.php
@@ -1,11 +1,14 @@
-<?php
+<?php 
 /**
-* This index file is part of the core of Niuware WebFramework 
-* and is not particularly intended to be modified.
-* For information about the license please visit the 
-* GIT repository at:
-* https://github.com/niuware/web-framework
-*/
+ * 
+ * This class is part of the core of Niuware WebFramework 
+ * and it is not particularly intended to be modified.
+ * For information about the license please visit the 
+ * GIT repository at:
+ * 
+ * https://github.com/niuware/web-framework
+ */
+
 namespace Niuware\WebFramework\Application;
 
 require_once 'vendor/autoload.php';
